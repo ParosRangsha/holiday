ce2051cfeff4584f8175f49c1563ffe1044a40a5
Paros Rangsha <paroscrangsha@gmail.com>
2025-03-23 14:24:25 +0000

    Footer update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,14 +1,21 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Container from './Container'
 import { menus } from '../mydata/Menudata'
 import { Link } from 'react-router-dom'
+import { Apidata } from '../mydata/ProductsApi'
 
 const Footer = () => {
   let mlist = useContext(menus)
+  let info = useContext(Apidata)
+  let [catelist, setcatelist] = useState([])
+  let [helplist, sethelplist] = useState(['Privacy Policy', 'Terms & Conditions', 'Special E-shop', 'Shipping', 'Secure Payments'])
+  useEffect(()=>{
+    setcatelist([...new Set(info.map((item) => item.category))]);
+  },[info, mlist])
   return (
     <div className="w-full bg-[#D8D8D8] py-[55px]">
       <Container>
-        <div className="w-1/2">
+        <div className="w-1/2 flex justify-between">
         <ul>
           {
             mlist.map((item, i)=>(
@@ -16,6 +23,21 @@ const Footer = () => {
             ))
           }
         </ul>
+        <ul>
+          {
+            catelist.map((item, i)=>(
+              <li key={i} className='py-[3px] cursor-pointer'>{item.toUpperCase()}</li>
+            ))
+          }
+        </ul>
+        <ul>
+          {
+            helplist.map((item, i)=>(
+              <li key={i}>{item}</li>
+            ))
+          }
+        </ul>
+
         </div>
       </Container>
     </div>
