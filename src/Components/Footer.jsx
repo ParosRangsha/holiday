1511b954859226e4376e05d44ef5caa93ed19b1f
Paros Rangsha <paroscrangsha@gmail.com>
2025-03-23 14:08:35 +0000

    Footer list add

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import Container from './Container'
+import { menus } from '../mydata/Menudata'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
+  let mlist = useContext(menus)
   return (
-    <div>Footer</div>
+    <div className="w-full bg-[#D8D8D8] py-[55px]">
+      <Container>
+        <div className="w-1/2">
+        <ul>
+          {
+            mlist.map((item, i)=>(
+              <li className='py-[3px]' key={i}><Link to={`${item}`}>{item}</Link></li>
+            ))
+          }
+        </ul>
+        </div>
+      </Container>
+    </div>
   )
 }
 
