0c09019e6123262d0605f0beb503a4a70e929e24
Paros Rangsha <paroscrangsha@gmail.com>
2025-04-11 09:50:31 +0000

    update 11 April 2025

--- src/pages/Productdetails.jsx
+++ src/pages/Productdetails.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import Container from '../Components/Container'
+import { FaPlus, FaMinus } from "react-icons/fa6";
 
 const Productdetails = () => {
     let productid = useParams()
@@ -15,9 +16,31 @@ const Productdetails = () => {
     useEffect(()=>{
         singledata()
     },[])
+
+    let [desShow, setDesShow] = useState(false)
+    let handleHide = ()=>{
+        setDesShow(false)
+    }
+    let handleShow = ()=>{
+        setDesShow(true)
+    }
+    let [settingShow, setSettingShow] = useState(true)
+    let handleSettingHide = ()=>{
+        setSettingShow(false)
+    }
+    let handleSettingShow = ()=>{
+        setSettingShow(true)
+    }
+    let [reportType, setreportType] = useState('rev')
+    let handleReprotDes = ()=>{
+        setreportType('des')
+    }
+    let handleReprotRev = ()=>{
+        setreportType('rev')
+    }
   return (
     <>
-    <div className="productDetails">
+    <div className="productDetails py-[30px]">
         <Container>
             <div className="heading">
                 <h1 className='text-[48px] font-bold'>Product Details</h1>
@@ -25,13 +48,45 @@ const Productdetails = () => {
             <div className="thumb w-full flex justify-center items-center">
                 <img src={singleProduct.thumbnail} alt="thumbnail" />
             </div>
-            <div className="details">
+            <div className="details flex flex-col gap-2">
                 <div className="rating">
                     <p>Rating: {singleProduct.rating} out of 5.00</p>
                 </div>
                 <div className="price flex gap-2">
                     <p>Price: ${singleProduct.price}</p>
                     <del>${Math.floor(singleProduct.price + 10)}.00</del>
+                </div>
+                <div className="flex items-center">
+                    <p className='font-bold'>Stock:</p> &nbsp;
+                    <p>In stock</p>
+                </div>
+                <div className="btns flex gap-2">
+                    <Link to='/wishlist' className='inline-block py-[10px] px-[15px] bg-[#000] text-[#fff] font-bold hover:bg-[#ccc] hover:text-[#002] border-[1px] hover:border-[#002] rounded-[5px]'>Add to Wish List</Link>
+                    <Link to='/cart' className='inline-block py-[10px] px-[15px] bg-[#000] text-[#fff] font-bold hover:bg-[#ccc] hover:text-[#002] border-[1px] hover:border-[#002] rounded-[5px]'>Add to Cart</Link>
+                </div>
+                <div className="details w-[600px] bg-[#ddd]">
+                    <div className="heading flex justify-between items-center">
+                        <h2 className='font-bold text-[20px] px-[10px]'>FEATURES  & DETAILS</h2>
+                        <div className="btns px-[10px]">
+                            {desShow ?<FaMinus onClick={()=>handleHide()}/> : <FaPlus onClick={()=>handleShow()}/>}
+                        </div>
+                    </div>
+                    {desShow && <p className='text-justify p-[10px]'>{singleProduct.description}</p>}
+                </div>
+                <div className="details w-[600px] bg-[#ddd]">
+                    <div className="heading flex justify-between items-center">
+                        <h2 className='font-bold text-[20px] px-[10px]'>SHIPPING & RETURNS</h2>
+                        <div className="btns px-[10px]">
+                            {settingShow ?<FaMinus onClick={()=>handleSettingHide()}/> : <FaPlus onClick={()=>handleSettingShow()}/>}
+                        </div>
+                    </div>
+                    {settingShow && <p className='text-justify p-[10px]'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic fugiat voluptatibus labore omnis, harum commodi unde eius numquam aut laborum.</p>}
+                </div>
+                <div className="description">
+                    <div className="desttitle flex gap-2">
+                        <p className={`${reportType == 'des' ? 'font-bold':'font-normal cursor-pointer'}`} onClick={()=>handleReprotDes()}>Description</p>
+                        <p className={`${reportType == 'rev' ? 'font-bold':'font-normal cursor-pointer'}`} onClick={()=>handleReprotRev()}>Reviews</p>
+                    </div>
                 </div>
             </div>
         </Container>
